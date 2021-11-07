9ec186ae572732fe7995e88ee6a99505ae27aabb
Lucas 洛可 <ycs77lucasv@gmail.com>
2021-11-08 02:32:16 +0800

    1-9 unplugin-icons - 快速使用 100+ 套 icon 套件庫 & Heroicons 介紹

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,6 @@
 <template>
   <div>Home</div>
-  <HelloWorld msg="Hello Lucas~" />
+  <!-- <IconHome class="w-8 h-8 text-violet-600" /> -->
+  <heroicons-outline-home class="w-8 h-8 text-violet-600" />
+  <heroicons-outline-user class="w-16 h-16 text-red-600" />
 </template>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +16,12 @@ export default defineConfig({
   plugins: [
     Vue(),
     Pages(),
-    Components(),
+    Components({
+      resolvers: IconsResolver({
+        prefix: false,
+        enabledCollections: ['heroicons-outline'],
+      }),
+    }),
+    Icons(),
   ],
 })
