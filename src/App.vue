80a7e78b93cfe3aafe9bc6c4f1d0380473513825
Lucas 洛可 <ycs77lucasv@gmail.com>
2021-12-13 04:10:58 +0800

    1-6 專案設定

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,7 +11,6 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+const colors = require('tailwindcss/colors')
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray': colors.zinc,
+      },
+      fontFamily: {
+        sans: ['Noto Sans TC', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
