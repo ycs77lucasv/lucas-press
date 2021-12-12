596e15c2970bd9c055b2b491c20db1b7e365519c
Lucas 洛可 <ycs77lucasv@gmail.com>
2021-12-13 03:43:58 +0800

    1-5 postcss-import - CSS 檔案模組化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,8 +23,12 @@ const count = ref(0)
       Vite Documentation
     </a>
     |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    <a href="https://v3.vuejs.org/" target="_blank" class="link">Vue 3 Documentation</a>
   </p>
+
+  <div class="card">
+    <a href="https://v3.vuejs.org/" target="_blank" class="link">Vue 3 Documentation</a>
+  </div>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
@@ -32,9 +36,3 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './styles/index.css'
 
 createApp(App).mount('#app')
